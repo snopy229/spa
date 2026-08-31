eca32bb3b7cceeccf40b6274e62d2ad6443f2d8d
snopy229 <jastcraft278@gmail.com>
2026-09-01 02:25:25 +0300

    add: model Comments

--- backend/src/comments/models.py
+++ backend/src/comments/models.py
@@ -1,1 +1,13 @@
 # Create your models here.
+from django.db import models
+
+
+class Comments(models.Model):
+    username = models.CharField(max_length=10)
+    avatar = models.ImageField(upload_to="avatar/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    email = models.EmailField(null=True, blank=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
