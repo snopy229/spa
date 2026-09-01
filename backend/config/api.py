3c021716fcb5c5471d8c667701427b70683407e9
snopy229 <jastcraft278@gmail.com>
2026-09-01 20:18:14 +0300

    add: ninja jwt to api

--- backend/config/api.py
+++ backend/config/api.py
@@ -1,6 +1,7 @@
-from ninja import NinjaAPI
-
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 from src.comments.api import router as comments_router
-api = NinjaAPI()
 
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 api.add_router('', comments_router)
