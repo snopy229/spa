385f392087b70be09348000d7250a47b84b443c0
snopy229 <jastcraft278@gmail.com>
2026-09-01 16:24:39 +0300

    fix: post_comment

--- backend/src/comments/api.py
+++ backend/src/comments/api.py
@@ -1,4 +1,4 @@
-from ninja import File, Form, Router, UploadedFile
+from ninja import Form, Router
 from src.comments.models import Comments
 from src.comments.schemas import CommentCreateIn, CommentTreeOut
 
@@ -17,18 +17,13 @@ def get_comments(request):
 
 
 @router.post("/comments", response=CommentTreeOut)
-def post_comment(
-    request,
-    payload: Form[CommentCreateIn],
-    avatar: UploadedFile | None = File(None),  # noqa: B008 # type: ignore
-    file: UploadedFile | None = File(None),  # noqa: B008 # type: ignore
-):
+def post_comment(request, payload: Form[CommentCreateIn]):
     comment = Comments.objects.create(
         username=payload.username,
         email=payload.email,
         text=payload.text,
-        avatar=avatar,
-        file=file,
+        avatar=payload.avatar,
+        file=payload.file,
         comment_id=payload.comment_id,
     )
 
