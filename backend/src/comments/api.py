068872aef8e8967a7ad0415fb43a2925945446a0
snopy229 <jastcraft278@gmail.com>
2026-09-01 20:09:32 +0300

    add: validators to api

--- backend/src/comments/api.py
+++ backend/src/comments/api.py
@@ -1,4 +1,4 @@
-from ninja import Form, Router
+from ninja import File, Form, Router, UploadedFile
 from src.comments.models import Comments
 from src.comments.schemas import CommentCreateIn, CommentTreeOut
 
@@ -17,14 +17,37 @@ def get_comments(request):
 
 
 @router.post("/comments", response=CommentTreeOut)
-def post_comment(request, payload: Form[CommentCreateIn]):
+def post_comment(
+    request,
+    payload: Form[CommentCreateIn],
+    file: File[UploadedFile] = None,  # type: ignore
+    avatar: File[UploadedFile] = None,
+):  # type: ignore
+
+    if file and file.content_type not in [
+        "text/plain",
+        "image/jpeg",
+        "image/gif",
+        "image/png",
+    ]:
+        raise ValueError("Разрешены только файлы формата TXT, JPG, GIF и PNG.")
+    if file.content_type == "text/plain" and file.size > 100 * 1024:
+        raise ValueError("Размер файла TXT не должен превышать 100 KB.")
+
+    if avatar and avatar.content_type not in ["image/jpeg", "image/gif", "image/png"]:
+        raise ValueError("Разрешены только файлы формата JPG, GIF и PNG.")
+
+    parent_id = (
+        payload.comment_id if payload.comment_id and payload.comment_id > 0 else None
+    )
+
     comment = Comments.objects.create(
         username=payload.username,
         email=payload.email,
         text=payload.text,
-        avatar=payload.avatar,
-        file=payload.file,
-        comment_id=payload.comment_id,
+        avatar=avatar,
+        file=file,
+        comment_id_id=parent_id,
     )
 
     return comment
