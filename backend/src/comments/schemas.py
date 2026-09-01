9f0551fc75c4e51362cb1d6688a9b07c46bb26e9
snopy229 <jastcraft278@gmail.com>
2026-09-01 16:30:43 +0300

    add: validate_file

--- backend/src/comments/schemas.py
+++ backend/src/comments/schemas.py
@@ -1,15 +1,34 @@
 from datetime import datetime
 
-from ninja import Schema
-from pydantic import EmailStr
+from ninja import Schema, UploadedFile
+from pydantic import EmailStr, field_validator
 
 
 class CommentCreateIn(Schema):
     username: str
     created_at: datetime
     text: str
+    avatar: UploadedFile | None = None
+    file: UploadedFile | None = None
     comment_id: int | None = None
     email: EmailStr | None = None
+
+    @field_validator("file")
+    @classmethod
+    def validate_file(cls, file: UploadedFile | None):
+        if file is None:
+            return file
+        if file.content_type not in [
+            "text/plain",
+            "image/jpeg",
+            "image/gif",
+            "image/png",
+        ]:
+            raise ValueError("Разрешены только файлы формата TXT, JPG, GIF и PNG.")
+        if file.content_type == "text/plain" and file.size > 100 * 1024:
+            raise ValueError("Размер файла TXT не должен превышать 100 KB.")
+
+        return file
 
 
 class CommentTreeOut(Schema):
