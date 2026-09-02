3ef4081267ad21f6ef3e5f3c27af0cb3f7b6fb53
snopy229 <jastcraft278@gmail.com>
2026-09-02 16:37:53 +0300

    add: validate  username

--- backend/src/comments/schemas.py
+++ backend/src/comments/schemas.py
@@ -1,8 +1,14 @@
+import re
 from datetime import datetime
 from typing import Literal
 
 from ninja import Schema
-from pydantic import EmailStr
+from pydantic import EmailStr, field_validator
+
+SORT_OPTIONS = Literal[
+    "username", "-username", "email", "-email", "created_at", "-created_at"
+]
+USERNAME_REGEX = re.compile(r"^[a-zA-Z0-9]+$")
 
 
 class CommentCreateIn(Schema):
@@ -10,6 +16,14 @@ class CommentCreateIn(Schema):
     text: str
     comment_id: int | None = None
     email: EmailStr | None = None
+
+    @field_validator("username")
+    def validate_username(cls, value: str) -> str:
+        if not USERNAME_REGEX.match(value):
+            raise ValueError(
+                "Имя пользователя может содержать только латинские буквы и цифры"
+            )
+        return value
 
 
 class CommentTreeOut(Schema):
@@ -22,8 +36,3 @@ class CommentTreeOut(Schema):
     email: EmailStr | None = None
     comment_id: int | None = None
     replies: list["CommentTreeOut"] = []  # noqa: RUF012
-
-
-SORT_OPTIONS = Literal[
-    "username", "-username", "email", "-email", "created_at", "-created_at"
-]
