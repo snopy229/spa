ec006063e9a4231daecdf783b13abdd2d61982a3
snopy229 <jastcraft278@gmail.com>
2026-09-02 16:11:53 +0300

    add: SORT_OPTIONS

--- backend/src/comments/schemas.py
+++ backend/src/comments/schemas.py
@@ -1,14 +1,13 @@
 from datetime import datetime
+from typing import Literal
 
-from ninja import Schema, UploadedFile
+from ninja import Schema
 from pydantic import EmailStr
 
 
 class CommentCreateIn(Schema):
     username: str
     text: str
-    avatar: UploadedFile | None = None
-    file: UploadedFile | None = None
     comment_id: int | None = None
     email: EmailStr | None = None
 
@@ -16,9 +15,15 @@ class CommentCreateIn(Schema):
 class CommentTreeOut(Schema):
     id: int
     username: str
-    avatar: str
-    file: str
+    avatar: str | None = None
+    file: str | None = None
     text: str
     created_at: datetime
     email: EmailStr | None = None
+    comment_id: int | None = None
     replies: list["CommentTreeOut"] = []  # noqa: RUF012
+
+
+SORT_OPTIONS = Literal[
+    "username", "-username", "email", "-email", "created_at", "-created_at"
+]
