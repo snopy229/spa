f82e013a0cb3b97f064205464063039e9a5baa9d
snopy229 <jastcraft278@gmail.com>
2026-09-03 21:22:12 +0300

    add: stati and media dirs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
