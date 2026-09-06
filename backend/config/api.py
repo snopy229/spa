abbaa4d2da21a34e58b2f56bd496d5409553e228
snopy229 <jastcraft278@gmail.com>
2026-09-06 17:57:00 +0300

    fix(config): restrict api docs to debug mode

--- backend/config/api.py
+++ backend/config/api.py
@@ -1,9 +1,10 @@
 from ninja import NinjaAPI
 
+from config import settings
 from src.default_exceptions import DefaultHTTPException
 from src.comments.api import router as comments_router
 
-api = NinjaAPI()
+api = NinjaAPI(docs_url="/docs" if settings.DEBUG else None)
 
 @api.exception_handler(DefaultHTTPException)
 def on_default_http_exception(request, exc: DefaultHTTPException):
